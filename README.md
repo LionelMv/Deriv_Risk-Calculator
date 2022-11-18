f2b81608b72d43ea6df7b124ed339445bdea583c
LionelMv <mvangicode@gmail.com>
2022-11-18 11:55:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Introduction
+This project implements a risk calculator for Deriv's synthetic indices (derived indices)
+Deriv is a broker that offers trading services for forex, stocks, indices, cryptocurrencies, commodities and derived (synthetic).
+Creating this risk calcuator for derived indices only is because other instruments have risk calculators that have been created for them. The other reason is that derived indices are fairly new and are only provided by Deriv broker.
+Access to Deriv broker:
+https://deriv.com/
+
+Volatiliy 50 (1s):
+- Lowest lot 0.005
+
---
+++ instruments/v501s.py
@@ -0,0 +1,18 @@
+print("**Risk Calculator for Volatility 50 (1s)**")
+lowest_lot = 0.005
+
+account_balance = float(input("Enter account balance: "))
+entry_price = float(input("Entry price: "))
+stop_price = float(input("Stop loss price: "))
+risk_percent = float(input("Enter risk in percentage: "))
+print()
+num_of_pips = entry_price - stop_price
+lowest_lot_amt = num_of_pips / 1000
+risk_amount = account_balance * risk_percent / 100
+lot_to_use = risk_amount / lowest_lot_amt * 0.001
+if lot_to_use <= 0.005:
+    print("The minimum lot you can use is 0.005")
+    print("Total risk = {:.2f}".format(lowest_lot_amt * 5))
+else:
+    print("The lot to use is: {:.4f}".format(lot_to_use))
+    print("Total risk = {:.2f}".format(num_of_pips * lot_to_use))
